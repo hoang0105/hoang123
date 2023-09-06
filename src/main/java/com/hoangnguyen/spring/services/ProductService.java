2d9b3af8a6803bb88395036902406acc7f730c8f
hoang <hoangn0105@gmail.com>
2023-09-06 16:03:50 +0700

    baiiiii

---
+++ src/main/java/com/hoangnguyen/spring/services/ProductService.java
@@ -0,0 +1,38 @@
+package com.hoangnguyen.spring.services;
+
+import com.hoangnguyen.spring.models.Product;
+import com.hoangnguyen.spring.repository.ProductRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Random;
+
+@Service
+public class ProductService {
+    @Autowired
+    ProductRepository repository;
+
+    public List<Product> getAll() {
+        return repository.getAll();
+    }
+
+    public Product getById(int id) {
+        return repository.getById(id);
+    }
+
+    public void insert(Product input) {
+        input.setId(new Random().nextInt(1000));
+        repository.insert(input);
+    }
+
+    public void update(Product input) {
+        repository.update(input);
+    }
+
+    public void delete(int id) {
+        repository.delete(id);
+    }
+}
